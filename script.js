9bd22972e78548d3f3812c3855c9db8a379bbb88
guarnerman <gs.ulises18@gmail.com>
2023-07-04 13:51:09 -0600

    Hello world JS, HTML

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const h1 = document.querySelector('h1')
-const form = document.querySelector('#formulario')
-const input1 = document.querySelector('#calculo1');
-const input2 = document.querySelector('#calculo2');
-const btn = document.querySelector('#boton-calcular');
-const pResult = document.querySelector('#resultado')
-
-form.addEventListener('submit', sumasInputValues);
-
-function sumasInputValues(event) {
-  /* onsole.log({event}); */
-  event.prevenDafault();
-  const sumaInput = input1.value + input2.value;
-  pResult.innerText = "Resultado: " + sumaInput; 
-}
+console.log("Hello world");
